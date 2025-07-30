ed7433e980863ee79a05df7693298dbcc01a249c
who.ayush <ayush06092002@gmail.com>
2025-07-31 01:48:18 +0530

    feat: update API endpoints to use production URL and enhance Celery SSL configuration

--- backend/app/tasks/celery_config.py
+++ backend/app/tasks/celery_config.py
@@ -5,13 +5,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CELERY_BROKER_URL = os.getenv("REDIS_URL")
-# print(CELERY_BROKER_URL)
 
 celery_app = Celery(
     "field_insights",
     broker=CELERY_BROKER_URL,
-    backend=CELERY_BROKER_URL  # Optional if you want to track task status
+    backend=CELERY_BROKER_URL
 )
+
+# Required SSL configs for rediss:// (Upstash)
+if CELERY_BROKER_URL.startswith("rediss://"):
+    ssl_config = {
+        "ssl_cert_reqs": "none"
+    }
+
+    celery_app.conf.broker_use_ssl = ssl_config
+    celery_app.conf.redis_backend_use_ssl = ssl_config
 
 celery_app.conf.task_routes = {
     "app.tasks.background_tasks.*": {"queue": "default"},
