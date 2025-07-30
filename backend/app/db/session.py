6fc243c71911a06dcf4c08b49cea46f8c56de906
who.ayush <ayush06092002@gmail.com>
2025-07-31 00:55:20 +0530

    refactor: update session management

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.models.db_models import Base
 import os
 from dotenv import load_dotenv
 
@@ -9,3 +10,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
